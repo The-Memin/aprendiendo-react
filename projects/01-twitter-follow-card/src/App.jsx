a344f40acfe85cd67433b93dbc9e6d82769a0f60
The-Memin <guillejuma@outlook.es>
2023-10-24 18:34:29 -0600

    Extract to components Tic tac toe

---
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css'
+import { TwitterFollowCard} from './TwitterFollowCard.jsx'
+
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Miguel Angel Duran',
+        isFollowing: true
+    },
+    {
+        userName: 'pheralb',
+        name: 'Pablo H.',
+        isFollowing: false
+    },
+    {
+        userName: 'PacoHdezs',
+        name: 'Paco Hdez',
+        isFollowing: true
+    },
+    {
+        userName: 'TMChein',
+        name: 'Tomas',
+        isFollowing: false
+    }
+]
+
+export function App() {
+    const formatUserName = (userName)=> `@${userName}`
+
+    return (
+        <section className='app'>
+            {
+                users.map(({userName, name, isFollowing})=>
+                    <TwitterFollowCard 
+                        key={userName}
+                        userName={userName} 
+                        initialIsFollowing={isFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard>
+                )
+            }
+        </section>
+    )
+}
